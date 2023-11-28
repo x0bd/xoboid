ded83293b7e65c6aa7586ab3ffdc38268f786cca
Tinodaishe Tembo <synthmancer@gmail.com>
2023-11-28 20:00:52 +0000

    wip

--- src/data/presentation.ts
+++ src/data/presentation.ts
@@ -1,36 +1,36 @@
 type Social = {
-	label: string;
-	link: string;
+  label: string;
+  link: string;
 };
 
 type Presentation = {
-	mail: string;
-	title: string;
-	description: string;
-	socials: Social[];
-	profile?: string;
+  mail: string;
+  title: string;
+  description: string;
+  socials: Social[];
+  profile?: string;
 };
 
 const presentation: Presentation = {
-	mail: "tinodatembo@gmail.com",
-	title: "こんにちは 👋, I’m Tinodaishe",
+  mail: "tinodatembo@gmail.com",
+  title: "こんにちは 👋, I’m Tinodaishe",
 
-	description:
-		"I am a *Software Engineer* with over *3 years* of experience in web development. I am currently working with *Next.js* and *TypeScript*. During my free time, I enjoy experimenting with *Three.js*, crafting playful computer things and learning Japanese.",
-	socials: [
-		{
-			label: "X",
-			link: "https://twitter.com/xoboid",
-		},
-		{
-			label: "LinkedIn",
-			link: "https://www.linkedin.com/in/tinodaishe-tembo-70aa3a266/",
-		},
-		{
-			label: "Github",
-			link: "https://github.com/x0bd",
-		},
-	],
+  description:
+    "I am a *Software Engineer* with over *3 years* of experience in web development. I am currently working with *Next.js*, *TypeScript* and *Rust*. During my free time, I enjoy experimenting with *Three.js*, crafting playful computer things and learning *Japanese*.",
+  socials: [
+    {
+      label: "X",
+      link: "https://twitter.com/xoboid",
+    },
+    {
+      label: "LinkedIn",
+      link: "https://www.linkedin.com/in/tinodaishe-tembo-70aa3a266/",
+    },
+    {
+      label: "Github",
+      link: "https://github.com/x0bd",
+    },
+  ],
 };
 
 export default presentation;
