52ec22a7eb44f49e3ea3c221181770c7c9e081b3
Tinodaishe Tembo <synthmancer@gmail.com>
2023-12-21 12:50:32 +0200

    wip

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,33 +1,38 @@
 export type Project = {
-  title: string;
-  techs: string[];
-  link: string;
-  isComingSoon?: boolean;
+	title: string;
+	techs: string[];
+	link: string;
+	isComingSoon?: boolean;
 };
 
 const projects: Project[] = [
-  {
-    title: "2048",
-    techs: ["React", "Vite", "JavaScript"],
-    link: "https://github.com/x0bd/2048",
-  },
-  {
-    title: "xoboid",
-    techs: ["Astro", "Tailwind", "zod", "TypeScript"],
-    link: "https://github.com/x0bd/xoboid",
-  },
-  {
-    title: "void.fm",
-    techs: ["Next.js", "Daisy/UI", "Express", "JavaScript"],
-    link: "/https://github.com/x0bd",
-    isComingSoon: true,
-  },
-  // {
-  //   title: "palegen",
-  //   techs: ["Next.js", "radix/ui", "zod", "Prisma", "TypeScript"],
-  //   link: "/",
-  //   isComingSoon: true,
-  // },
+	{
+		title: "tetrix",
+		techs: ["Next.js", "TailwindCSS", "TypeScript"],
+		link: "https://github.com/x0bd/2048",
+	},
+	{
+		title: "task.sh",
+		techs: ["Next.js", "shadcn/ui", "TypeScript", "Prisma", "Clerk", "zod"],
+		link: "https://github.com/x0bd/hyper-tasks",
+	},
+	{
+		title: "xoboid",
+		techs: ["Astro", "TailwindCSS", "TypeScript"],
+		link: "https://github.com/x0bd/xoboid",
+	},
+	{
+		title: "void.stats",
+		techs: ["Next.js", "Daisy/UI", "Express", "JavaScript"],
+		link: "/https://github.com/x0bd",
+		isComingSoon: true,
+	},
+	// {
+	//   title: "palegen",
+	//   techs: ["Next.js", "radix/ui", "zod", "Prisma", "TypeScript"],
+	//   link: "/",
+	//   isComingSoon: true,
+	// },
 ];
 
 // {
