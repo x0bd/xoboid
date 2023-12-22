1e4d1988622d7d5e124e12c232c84b9b14a5a271
Tinodaishe Tembo <synthmancer@gmail.com>
2023-12-22 22:21:26 +0200

    grateful

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -7,24 +7,24 @@ export type Project = {
 
 const projects: Project[] = [
 	{
-		title: "tetrix",
-		techs: ["Next.js", "Daisy-ui", "TypeScript"],
-		link: "https://github.com/x0bd/2048",
-	},
-	{
-		title: "lembe.sh",
-		techs: ["Next.js", "Shadcn/ui", "TypeScript", "Prisma", "Zod"],
-		link: "https://github.com/x0bd/lembe",
-	},
-	{
 		title: "xoboid",
 		techs: ["Astro", "TailwindCSS", "TypeScript"],
 		link: "https://github.com/x0bd/xoboid",
 	},
 	{
+		title: "tetrix",
+		techs: ["Next.js", "Daisy-ui", "TypeScript", "Vitest"],
+		link: "https://github.com/x0bd/2048",
+	},
+	{
 		title: "palegen",
 		techs: ["Next.js", "Daisy-ui", "TypeScript"],
 		link: "/https://github.com/x0bd",
+	},
+	{
+		title: "lembe.sh",
+		techs: ["Next.js", "Shadcn/ui", "TypeScript", "Prisma", "Zod"],
+		link: "https://github.com/x0bd/lembe",
 		isComingSoon: true,
 	},
 	// {
