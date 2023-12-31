4c4f11a1bfa88d253c1feeb05ce285bfab43e2b1
Tinodaishe Tembo <synthmancer@gmail.com>
2023-12-31 11:56:39 +0200

    こんにちは

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -7,15 +7,21 @@ export type Project = {
 
 const projects: Project[] = [
 	{
-		title: "tetrix",
-		techs: ["React", "Tailwind CSS", "TypeScript", "Vitest"],
-		link: "https://github.com/x0bd/tetrix",
+		title: "xoboid",
+		techs: ["Astro", "Tailwind CSS", "TypeScript"],
+		link: "https://github.com/x0bd/xoboid",
 	},
 	{
 		title: "palegen",
 		techs: ["Next.js", "Shadcn/ui", "TypeScript"],
 		link: "/https://github.com/palegen",
 	},
+	{
+		title: "tetrix",
+		techs: ["React", "Tailwind CSS", "TypeScript", "Vitest"],
+		link: "https://github.com/x0bd/tetrix",
+	},
+
 	{
 		title: "lembe",
 		techs: ["Next.js", "Shadcn/ui", "TypeScript", "Prisma", "Zod", "MySQL"],
