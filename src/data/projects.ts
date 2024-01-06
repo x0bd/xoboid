7e11ecaaac52a83ceaeef6e2874bb2975529e066
Tinodaishe Tembo <synthmancer@gmail.com>
2024-01-06 14:58:23 +0200

    wip

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -9,8 +9,8 @@ export type Project = {
 
 const projects: Project[] = [
 	{
-		title: "ambient",
-		techs: ["Ambient", "Vite", "TypeScript"],
+		title: "celeste.js",
+		techs: ["Celeste.js", "Vite", "TypeScript"],
 		description: "a minimal frontend web framework",
 		link: "https://github.com/x0bd/redium",
 		promoted: true,
@@ -18,24 +18,46 @@ const projects: Project[] = [
 
 	{
 		title: "lembe",
-		techs: ["Next.js", "Shadcn/ui", "TypeScript", "Prisma", "Zod", "MySQL"],
-		description: "entertainment ecosystem manager",
+		techs: [
+			"Next.js",
+			"Shadcn/ui",
+			"TypeScript",
+			"Prisma",
+			"Zustand",
+			"Zod",
+		],
+		description: "an entertainment ecosystem manager",
 		link: "https://github.com/x0bd/lembe",
 		promoted: true,
 	},
 	{
 		title: "doodle.px",
-		techs: ["Next.js", "Tailwind CSS", "TypeScript"],
+		techs: ["Next.js", "Shadcn/ui", "TypeScript"],
 		description: "a tiny pixel art editor",
 		link: "/",
 		promoted: false,
 	},
 	{
 		title: "palegen",
-		techs: ['Next.js', 'Shadcn/ui', 'TypeScript'],
-		description: 'generate color palettes from images',
-		link: '/',
-		promoted: true
+		techs: ["Next.js", "Shadcn/ui", "TypeScript"],
+		description: "generate color palettes from images",
+		link: "/",
+		promoted: true,
+	},
+	{
+		title: "wave.css",
+		techs: ["Vite", "JavaScript", "SCSS"],
+		description: "a lightweight and fast css framework",
+		link: "/",
+		promoted: false,
+	},
+	{
+		title: "celeste ui",
+		techs: ["React", "TypeScript", "Vite"],
+		description: "an accessible react ui component library",
+		link: "/",
+		promoted: false,
+		isComingSoon: false,
 	},
 	{
 		title: "xoboid",
@@ -51,6 +73,27 @@ const projects: Project[] = [
 		link: "https://github.com/x0bd/void-fm",
 		promoted: true,
 		isComingSoon: true,
+	},
+	{
+		title: "guestbook",
+		techs: ["Next.js", "Shadcn/ui", "TypeScript", "Supabase"],
+		description: "a guestbook project for your personal site",
+		link: "/",
+		promoted: false,
+	},
+	{
+		title: "ambient-rose",
+		description: "a brutal version of the rose-pine vs-code theme",
+		techs: ["JavaScript"],
+		link: "/",
+		promoted: false,
+	},
+	{
+		title: "void.fy",
+		techs: ["Next.js", "Shadcn/ui", "TypeScript", "Spotify"],
+		description: "a spotify statis app",
+		link: "/",
+		promoted: false,
 	},
 ];
 
