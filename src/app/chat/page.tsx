f3502b6369d833d6c5220c53d8707eea181d7728
Tinodaishe Tembo <synthmancer@gmail.com>
2024-01-17 22:14:59 +0200

    wip

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -2,7 +2,7 @@ const Chat = () => {
 	return (
 		<main className="flex flex-col gap-20">
 			<h1 className="text-3xl font-medium">
-				Hello Strangers, you can chat here, still a WIP.
+				Hello Strangers, you can chat here.
 			</h1>
 		</main>
 	);
