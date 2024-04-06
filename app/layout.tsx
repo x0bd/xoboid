f5a059005194465d63ea322e171bddf45494ab0c
Tinodaishe Tembo <synthmancer@gmail.com>
2024-04-06 19:34:58 +0200

    add header

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,12 @@
+"use client";
+
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { motion } from "framer-motion";
+import Header from "@/components/Header";
 
 const inter = Inter({ subsets: ["latin"] });
-
-export const metadata: Metadata = {
-	title: "xoboid",
-	description: "i own a computer",
-};
 
 export default function RootLayout({
 	children,
@@ -17,7 +15,10 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en">
-			<motion.body className="bg-white top-24 relative dark:bg-[#212121] shadow-md rounded-xl max-w-xl lg:mx-auto mx-4 lg:max-w-xl  max-2xl  md:mx-auto sm:mx-auto lg:px-0"></motion.body>
+			<body className={inter.className}>
+				<Header />
+				{children}
+			</body>
 		</html>
 	);
 }
