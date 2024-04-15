1711e2c3eba1f5b784cc10087a7ae8f50c27ffa6
Tinodaishe Tembo <synthmancer@gmail.com>
2024-04-15 11:49:19 +0200

    about

---
+++ src/data/about.ts
@@ -0,0 +1,48 @@
+type tool = {
+	name: string;
+	description: string;
+	link: string;
+};
+
+export const tools: tool[] = [
+	{
+		name: "Obsidian",
+		description: "My Second brain",
+		link: "",
+	},
+	{
+		name: "VS Code",
+		description: "my primary code editor",
+		link: "",
+	},
+	{
+		name: "Terminal",
+		description: "my favorite terminal on Windows",
+		link: "",
+	},
+	{
+		name: "Eagle",
+		description: "a tool i use to collect and manage visual inspiration",
+		link: "",
+	},
+	{
+		name: "Starship.rs",
+		description: "Terminal Prompt Engine",
+		link: "",
+	},
+	{
+		name: "Numi",
+		description: "Swiss Army Knife Calculator",
+		link: "",
+	},
+	{
+		name: "Hoppscotch",
+		description: "API Testing tool",
+		link: "",
+	},
+	{
+		name: "TablePlus",
+		description: "database management tool",
+		link: "",
+	},
+];
