15ae86a6fb96e644fcb179307485afde4dd9a25e
Tinodaishe Tembo <synthmancer@gmail.com>
2024-04-23 19:41:47 +0200

    chore: work on experiments page

--- src/data/consts.ts
+++ src/data/consts.ts
@@ -6,7 +6,7 @@ export const SITE: Site = {
 	NUM_POSTS_ON_HOMEPAGE: 2,
 	NUM_WORKS_ON_HOMEPAGE: 2,
 	NUM_PROJECTS_ON_HOMEPAGE: 3,
-	NUM_EXPERIMENTS_ON_HOMEPAGE: 3,
+	NUM_EXPERIMENTS_ON_HOMEPAGE: 2,
 };
 
 export const HOME: Metadata = {
@@ -33,6 +33,11 @@ export const PROJECTS: Metadata = {
 export const MUSIC: Metadata = {
 	TITLE: "music",
 	DESCRIPTION: "A collection of my favorite tracks and artists from spotify",
+};
+
+export const EXPERIMENTS: Metadata = {
+	TITLE: "experiments",
+	DESCRIPTION: "art, doodles and generative stuff",
 };
 
 export const SOCIALS: Socials = [
