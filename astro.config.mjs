690ee0b8b9117ffccb2d2ace3f1a70d6042a58ae
Tinodaishe Tembo <synthmancer@gmail.com>
2024-04-27 11:57:18 +0200

    soho

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
 	markdown: {
 		shikiConfig: {
-			theme: "vitesse-dark",
+			theme: "vesper",
 		},
 	},
 	site: "https://xoboid.vercel.app",
