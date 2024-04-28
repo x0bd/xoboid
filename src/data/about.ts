aa8d850cfe0551bc42986f16a69edc746efe93dc
Tinodaishe Tembo <synthmancer@gmail.com>
2024-04-28 21:14:42 +0200

    design engineer

---
+++ src/data/about.ts
@@ -0,0 +1,63 @@
+type tool = {
+	name: string;
+	description: string;
+	link: string;
+};
+
+export const tools: tool[] = [
+	{
+		name: "obsidian",
+		description: "my second brain",
+		link: "https://obsidian.md/",
+	},
+	{
+		name: "neovim",
+		description: "quickly turning into my favorite text editor",
+		link: "https://code.visualstudio.com/",
+	},
+	{
+		name: "terminal",
+		description: "a modern terminal emulator for windows",
+		link: "https://github.com/microsoft/terminal",
+	},
+	{
+		name: "eagle",
+		description: "collect and manage visual inspiration",
+		link: "https://eagle.cool",
+	},
+	{
+		name: "starship",
+		description: "terminal prompt engine",
+		link: "https://starship.rs",
+	},
+	{
+		name: "numi",
+		description: "a beautiful swiss army calculator",
+		link: "numi.app",
+	},
+	{
+		name: "gemini",
+		description: "my personal assistant",
+		link: "https://gemini.google.com",
+	},
+	{
+		name: "todoist",
+		description: "task management",
+		link: "https://todoist.com",
+	},
+	{
+		name: "blender",
+		description: "3d creation suite",
+		link: "https://blender.org",
+	},
+	{
+		name: "figma",
+		description: "design and prototyping tool",
+		link: "figma.com",
+	},
+	{
+		name: "hoppscotch",
+		description: "api testing platform",
+		link: "https://hoppscotch.org",
+	},
+];
