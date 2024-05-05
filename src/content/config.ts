1e8e713bc17b5d3a89d27f897a3f597e8a0b8392
Tinodaishe Tembo <synthmancer@gmail.com>
2024-05-05 16:01:16 +0200

    wip remove placeholder fun images

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,5 +1,7 @@
 import { defineCollection, z } from "astro:content";
 
+
+
 const blog = defineCollection({
 	type: "content",
 	schema: z.object({
--- src/data/fun.ts
+++ src/data/fun.ts
@@ -3,7 +3,6 @@ export type experiment = {
 	previewFile: string;
 	sourceLink: string;
 	description: string;
-	date: string;
 	stack: string;
 	fileType: "img" | "vid";
 };
@@ -14,17 +13,7 @@ export const experiments: experiment[] = [
 		sourceLink: "",
 		previewFile: "/fun/pleasure.gif",
 		description: "A recreation of the iconic Joy Division Album Art Cover",
-		date: "Jan 10 2024",
 		stack: "p5.js",
-		fileType: "img",
-	},
-	{
-		title: "Neuromancer",
-		sourceLink: "",
-		previewFile: "/fun/Neuromancer.jpeg",
-		description: "My favorite from the sprawl trilogy",
-		date: "31 Oct 2021",
-		stack: "blender",
 		fileType: "img",
 	},
 	{
@@ -32,7 +21,6 @@ export const experiments: experiment[] = [
 		sourceLink: "",
 		previewFile: "fun/psylock.jpg",
 		description: "My Childhood MILF crush",
-		date: "31 Oct 2023",
 		stack: "blender",
 		fileType: "img",
 	},
@@ -41,7 +29,6 @@ export const experiments: experiment[] = [
 		sourceLink: "",
 		previewFile: "fun/morty.png",
 		description: "A Terminal Ascii Art Generator Written In Rust",
-		date: "01 May 2024",
 		stack: "rust",
 		fileType: "img",
 	},
@@ -51,17 +38,7 @@ export const experiments: experiment[] = [
 		previewFile: "/fun/3body.gif",
 		description:
 			"Recently finished reading the books and watching the show and wanted to visualize this fascinating physics problem",
-		date: "26 Mar 2024",
 		stack: "p5.js",
-		fileType: "img",
-	},
-	{
-		title: "The Adjudicator",
-		sourceLink: "",
-		previewFile: "fun/wick.jpg",
-		description: "my other badass crush",
-		date: "27 Sep 2023",
-		stack: "photograph",
 		fileType: "img",
 	},
 ];
