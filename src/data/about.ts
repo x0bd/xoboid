943d54ad57f052883bec876aa5b0dbaeaba6ab42
Tinodaishe Tembo <synthmancer@gmail.com>
2024-05-09 20:56:18 +0200

    wip

--- src/data/about.ts
+++ src/data/about.ts
@@ -4,124 +4,6 @@ type tool = {
 	link: string;
 };
 
-// Work on this to include icons
-type tech = {
-	name: string;
-	link: string;
-};
-
-// Programming Languages
-export const languages: tech[] = [
-	{
-		name: "HTML/CSS",
-		link: "",
-	},
-	{
-		name: "JavaScript",
-		link: "",
-	},
-	{
-		name: "TypeScript",
-		link: "",
-	},
-	{
-		name: "Rust",
-		link: "",
-	},
-	{
-		name: "GLSL",
-		link: "",
-	},
-];
-
-// Libraries & Frameworks
-export const frameworks: tech[] = [
-	{
-		name: "React",
-		link: "",
-	},
-	{
-		name: "Next.js",
-		link: "",
-	},
-	{
-		name: "Astro",
-		link: "https://astro.dev",
-	},
-	{
-		name: "Expo",
-		link: "",
-	},
-	{
-		name: "Tauri",
-		link: "",
-	},
-	{
-		name: "Three.js",
-		link: "",
-	},
-	{
-		name: "React Three Fiber",
-		link: "",
-	},
-	{
-		name: "Zustand",
-		link: "",
-	},
-	{
-		name: "TailwindCSS",
-		link: "",
-	},
-	{
-		name: "Framer Motion",
-		link: "",
-	},
-	{
-		name: "GSAP",
-		link: "",
-	},
-	{
-		name: "P5.js",
-		link: "",
-	},
-];
-
-// Tools & Platforms
-export const platforms: tech[] = [
-	{
-		name: "Vercel",
-		link: "vercel.com",
-	},
-	{
-		name: "Git/Github",
-		link: "",
-	},
-	{
-		name: "Supabase",
-		link: "",
-	},
-	{
-		name: "Vite/Vitest",
-		link: "",
-	},
-	{
-		name: "Bun",
-		link: "",
-	},
-	{
-		name: "Node.js",
-		link: "",
-	},
-	{
-		name: "Prisma",
-		link: "",
-	},
-	{
-		name: "Webpack",
-		link: "",
-	},
-];
-
 export const tools: tool[] = [
 	{
 		name: "obsidian",
@@ -174,22 +56,8 @@ export const tools: tool[] = [
 		link: "https://figma.com",
 	},
 	{
-		name: "rosé pine",
-		description:
-			"gorgeous color palette with a bit of soho vibes for the classy minimalist",
+		name: "vitesse",
+		description: "a modern take on the gruvbox theme",
 		link: "https://rose-pine.org",
 	},
 ];
-
-const signals = [
-	"dreams are messages from the deep",
-	"signals from the void",
-	"stay hungry, stay foolish",
-	"oss/acc",
-	"the thermodynamic god",
-];
-
-export const randomSignal = () => {
-	const seed = Math.floor(Math.random() * signals.length);
-	return signals[seed];
-};
--- src/data/consts.ts
+++ src/data/consts.ts
@@ -1,4 +1,4 @@
-import type { Site, Metadata, Socials, techs } from "@types";
+import type { Site, Metadata, Socials } from "@types";
 
 export const SITE: Site = {
 	NAME: "xoboid",
@@ -53,41 +53,3 @@ export const SOCIALS: Socials = [
 		HREF: "/resume.pdf",
 	},
 ];
-
-// Modify or add relevant techs
-export const technologies: techs = {
-	astro: "devicon-plain:astro",
-	nextjs: "akar-icons:nextjs-fill",
-	tailwind: "mdi:tailwind",
-	remix: "simple-icons:remix",
-	stripe: "fa-brands:stripe",
-	figma: "ph:figma-logo",
-	svelte: "simple-icons:svelte",
-	prisma: "simple-icons:prisma",
-	supabase: "ri:supabase-fill",
-	webgl: "simple-icons:webgl",
-	threejs: "logos:threejs",
-	p5js: "simple-icons:p5dotjs",
-	jotai: "logos:jotai",
-	electron: "simple-icons:electron",
-	sass: "simple-icons:sass",
-	vite: "simple-icons:vite",
-	bun: "devicon-plain:bun",
-	planetscale: "skill-icons:planetscale-light",
-	vercel: "akar-icons:vercel-fill",
-	typeScript: "simple-icons:typescript",
-	javaScript: "simple-icons:javascript",
-	blender: "simple-icons:blender",
-	shadcn: "simple-icons:shadcnui",
-	react: "simple-icons:react",
-	socketio: "cib:socket-io",
-	drizzle: "simple-icons:drizzle",
-	zod: "simple-icons:zod",
-	radix: "simple-icons:radixui",
-	node: "akar-icons:node-fill",
-	express: "simple-icons:express",
-	mdx: "simple-icons:mdx",
-	spotify: "bi:spotify",
-	rust: "simple-icons:rust",
-	framer_motion: "tabler:brand-framer-motion",
-};
