685e84031a84e16adfd6ff91d82dc569aa62b5fb
Tinodaishe Tembo <synthmancer@gmail.com>
2024-07-03 18:39:50 +0200

    config astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,15 +3,23 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
+import vercel from "@astrojs/vercel/serverless";
 
-// https://astro.build/config
 export default defineConfig({
 	markdown: {
 		shikiConfig: {
 			theme: "vesper",
 		},
 	},
-
 	site: "https://xoboid.vercel.app",
 	integrations: [mdx(), sitemap(), tailwind(), icon()],
+	output: "hybrid",
+	adapter: vercel(),
+	server: {
+		port: 8600,
+	},
+	prefetch: {
+		prefetchAll: true,
+		defaultStrategy: "viewport",
+	},
 });
